c95beed1ba5e1dedb4f8e97f05e3c207391084aa
maoyifeng <maoyifeng@evergrande.cn>
2019-01-03 08:41:30 +0800

    Initial Commit

---
+++ iot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iot
+//
+//  Created by Genobili Mao on 2019/1/3.
+//  Copyright © 2019 Genobili Mao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
